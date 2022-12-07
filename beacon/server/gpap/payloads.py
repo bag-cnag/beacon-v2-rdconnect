862a6365427ada43e32a7041e32db3e9780879f8
taspapak <taspapak@hotmail.com>
2022-12-07 17:26:49 +0100

    filters setters

--- beacon/server/gpap/payloads.py
+++ beacon/server/gpap/payloads.py
@@ -10,6 +10,56 @@ import re
 # _valid_individuals = [ 'id', 'family_id', 'index', 'solved', 'sex', 'affectedStatus', 'lifeStatus' ]
 # _valid_biosamples  = [ 'RD_Connect_ID_Experiment', 'EGA_ID', 'Participant_ID', 'Owner', 'in_platform', 'POSTEMBARGO', 'experiment_type', 'kit', 'tissue', 'library_source', 'library_selection', 'library_strategy', 'library_contruction_protocol', 'erns' ]
 
+def set_hpo(item):
+    """ Set HPO filter """
+    hpo = {}
+    if ("type" in item) and ((item["type"] == "SIO_010056") or (item["type"] == "sio:SIO_010056")) and ((item["id"].lower().startswith('hp')) or (item["id"].lower().startswith('obo:hp'))):
+        hpo_string = "HP:" + re.split('[_ :]', item["id"])[-1]
+        hpo = { 'id': 'features', 'value': hpo_string }
+
+    return hpo
+
+def set_ordo(item):
+    """ Set ORDO filter """
+    ordo = {}
+    if ("type" in item) and ((item["type"] == "SIO_001003") or (item["type"] == "sio:SIO_001003")) and ((item["id"].lower().startswith('orpha')) or (item["id"].lower().startswith('ordo:orpha'))):
+        ordo_string = "Orphanet:" + re.split('[_ :]', item["id"])[-1]
+        ordo = { 'id': 'diagnosis', 'value': ordo_string }
+    
+    return ordo
+
+def set_omim(item):
+    """ Set OMIM filter """
+    omim = {}
+    if ("type" in item) and ((item["type"] == "SIO_001003") or (item["type"] == "sio:SIO_001003")) and (item["id"].lower().startswith('omim')):
+        omim_string = "OMIM:" + re.split('[_ :]', item["id"])[1]
+        omim = { 'id': 'disorders', 'value': omim_string }
+    
+    return omim
+
+def set_gene(item):
+    """ Set gene filter """
+    gene = {}
+    if ('type' in item) and ((item['type'] == 'NCIT_C16612') or (item["type"] == "obo:NCIT_C16612")):
+        gene = { 'id': 'genes', 'value': item["id"] }
+    
+    return gene
+
+def set_sex(item):
+    """ Set sex filter """
+    sex = {}
+    if ("type" in item) and ((item["type"] == "NCIT_C28421") or (item["type"] == "obo:NCIT_C28421")):
+        if item["id"] == 'NCIT_C16576' or item["id"] == 'obo:NCIT_C16576': # female
+            sex = { 'id': 'sex', 'value': 'F' }
+        if item["id"] == 'NCIT_C20197' or item["id"] == 'obo:NCIT_C20197': # male
+            sex = { 'id': 'sex', 'value': 'M' }
+        if item["id"] == 'NCIT_C124294' or item["id"] == 'obo:NCIT_C124294': # unknown
+            sex = { 'id': 'sex', 'value': 'U' }
+        if item["id"] == 'NCIT_C17998' or item["id"] == 'obo:NCIT_C17998': # unknown
+            sex = { 'id': 'sex', 'value': 'U' }
+    
+    return sex
+
 
 # Function to translate from RequestParameters to PhenoStore filtering
 def ps_to_gpap( qparams, psid = None ):
@@ -20,33 +70,47 @@ def ps_to_gpap( qparams, psid = None ):
         for item in qparams[ 'query' ][ 'filters' ]:
 
             #HPOs
-            if ("type" in item) and (item["type"] == "SIO_010056") and (item["id"].startswith('HP')):
-                fltrs.append ({ 'id': 'features', 'value': item["id"] } )
+            #if ("type" in item) and ((item["type"] == "SIO_010056") or (item["type"] == "sio:SIO_010056")) and ((item["id"].lower().startswith('hp')) or (item["id"].lower().startswith('obo:hp'))):
+            #    hpo_string = "HP:" + re.split('[_ :]', item["id"])[-1]
+            #    fltrs.append ({ 'id': 'features', 'value': hpo_string } )
 
             #ORDO
-            if ("type" in item) and (item["type"] == "SIO_001003") and (item["id"].lower().startswith('orpha')):
-                ordo_string = "Orphanet:" + re.split('[_ :]', item["id"])[1]
-                fltrs.append ({ 'id': 'diagnosis', 'value': ordo_string } )
+            #if ("type" in item) and ((item["type"] == "SIO_001003") or (item["type"] == "sio:SIO_001003")) and ((item["id"].lower().startswith('orpha')) or (item["id"].lower().startswith('ordo:orpha'))):
+            #    ordo_string = "Orphanet:" + re.split('[_ :]', item["id"])[-1]
+            #    fltrs.append ({ 'id': 'diagnosis', 'value': ordo_string } )
 
             #OMIM
-            if ("type" in item) and (item["type"] == "SIO_001003") and (item["id"].lower().startswith('omim')):
-                omim_string = "OMIM:" + re.split('[_ :]', item["id"])[1]
-                fltrs.append ({ 'id': 'disorders', 'value': omim_string } )
+            #if ("type" in item) and ((item["type"] == "SIO_001003") or (item["type"] == "sio:SIO_001003")) and (item["id"].lower().startswith('omim')):
+            #    omim_string = "OMIM:" + re.split('[_ :]', item["id"])[1]
+            #    fltrs.append ({ 'id': 'disorders', 'value': omim_string } )
 
             #Sex
-            if ("type" in item) and (item["type"] == "NCIT_C28421"):
-                if item["id"] == 'NCIT_C16576': # female
-                    fltrs.append ({ 'id': 'sex', 'value': 'F' } )
-                if item["id"] == 'NCIT_C20197': # male
-                    fltrs.append ({ 'id': 'sex', 'value': 'M' } )
-                if item["id"] == 'NCIT_C124294': # unknown
-                    fltrs.append ({ 'id': 'sex', 'value': 'U' } )
-                if item["id"] == 'NCIT_C17998': # unknown
-                    fltrs.append ({ 'id': 'sex', 'value': 'U' } )
+            #if ("type" in item) and ((item["type"] == "NCIT_C28421") or (item["type"] == "obo:NCIT_C28421")):
+            #    if item["id"] == 'NCIT_C16576' or item["id"] == 'obo:NCIT_C16576': # female
+            #        fltrs.append ({ 'id': 'sex', 'value': 'F' } )
+            #    if item["id"] == 'NCIT_C20197' or item["id"] == 'obo:NCIT_C20197': # male
+            #        fltrs.append ({ 'id': 'sex', 'value': 'M' } )
+            #    if item["id"] == 'NCIT_C124294' or item["id"] == 'obo:NCIT_C124294': # unknown
+            #        fltrs.append ({ 'id': 'sex', 'value': 'U' } )
+            #    if item["id"] == 'NCIT_C17998' or item["id"] == 'obo:NCIT_C17998': # unknown
+            #        fltrs.append ({ 'id': 'sex', 'value': 'U' } )
 
             #Genes
-            if ('type' in item) and (item['type'] == 'NCIT_C16612'):
-                fltrs.append ({ 'id': 'genes', 'value': item["id"] } )
+            #if ('type' in item) and ((item['type'] == 'NCIT_C16612') or (item["type"] == "obo:NCIT_C16612")):
+            #    fltrs.append ({ 'id': 'genes', 'value': item["id"] } )
+
+            
+            hpo_fltr = set_hpo(item)
+            ordo_fltr = set_ordo(item)
+            omim_fltr = set_omim(item)
+            gene_fltr = set_gene(item)
+            sex_fltr = set_sex(item)
+            
+            if hpo_fltr:  fltrs.append(hpo_fltr)
+            if ordo_fltr: fltrs.append(ordo_fltr)
+            if omim_fltr: fltrs.append(omim_fltr)
+            if gene_fltr: fltrs.append(gene_fltr)
+            if sex_fltr:  fltrs.append(sex_fltr)
 
         #If nothing from the above applies
         if len(fltrs) == 0:
@@ -54,7 +118,8 @@ def ps_to_gpap( qparams, psid = None ):
     
     else:
         fltrs.append ({ 'id': '_no_filter', 'value': '_no_filter' } )
-
+    
+    print (fltrs)
     return fltrs
 
 # Function to translate from RequestParameters to DataManagement filtering
